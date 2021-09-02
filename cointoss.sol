dec6118b1c0287e5d46e9e8b4df671d0e8ed82bc
Jon Dreksler <jondreksler@sideways8creations.com>
2021-09-01 17:32:23 -0700

    main source

---
+++ cointoss.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.1;
+
+contract Coin
+{
+    //contract owners 
+    uint256 amount;
+    uint256 blockNumber;
+    address payable[] bettors;
+contructor(uint256 amount_) public
+{
+amount = amount_;   //creates contract and bet amount in wei
+}
+//place bet
+function bet() payable public 
+{
+   require(msg.value == amount);
+   require(bettors.length < 2);
+   blockNumber = block.number + 1;
+   bettors.push(msg.sender);
+}
+//Tosses Coin Pays Winner
+function toss()public
+{
+    require(bettors.length == 2);
+    require(blockNumber < block.number);
+    uint256 winner = uint256(blockhash(block.number)) % 2;
+    bettors[winner].transfer(address(this).balance);
+
+}
